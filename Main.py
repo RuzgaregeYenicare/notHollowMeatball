7fe152931330a680fd01999e23e8dffdf9a76a79
Cigkofte <ruzgarege.yenicare@gmail.com>
2025-10-11 17:32:13 +0300

    main loop and settings added

--- Main.py
+++ Main.py
@@ -1,3 +1,27 @@
-import pygame
+import pygame, sys
+from pygame import display, time
 
-print("hi")
+from settings import *
+
+class main:
+    def __init__(self):
+
+        pygame.init()
+
+        self.screen = display.set_mode((screenWidth, screenHeigth))
+        self.clock = time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            self.screen.fill("black")
+            display.update()
+            self.clock.tick(FPS)
+
+if __name__ == "__main__":
+    window = main()
+    window.run()
